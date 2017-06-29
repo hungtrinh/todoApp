3376522974c0d1c9812d342373360f77cb924327
hungtd <trinhduchung@gmail.com>
2017-06-29 14:50:20 +0700

    Spec create new task by task desc

--- app/features/task/task.js
+++ app/features/task/task.js
@@ -1,8 +1,29 @@
 'use strict'
 
-exports.create = (desc = '') => Object.freeze({
-  description: desc,
-  createdAt: Date.now(),
-  updatedAt: Date.now(),
-  completed: false
-})
+/**
+ * Create an task
+ *
+ * @param {object} | string task
+ * @param {string} task.description
+ * @param {Date} task.createdAt
+ * @param {Date} task.updatedAt
+ * @param {boolean} task.completed
+ *
+ * @return {object} {description, createdAt, updatedAt, completed}
+ */
+exports.create = (task) => {
+  const today = Date.now()
+  const {
+    description,
+    createdAt,
+    updatedAt,
+    completed
+  } = (typeof task === 'string') ? {description: task} : task
+
+  return Object.freeze({
+    description: description || '',
+    createdAt: createdAt || today,
+    updatedAt: updatedAt || today,
+    completed: completed || false
+  })
+}
--- app/features/task/task.test.js
+++ app/features/task/task.test.js
@@ -1,14 +1,22 @@
 'use strict'
 
 const test = require('tape-promise/tape')
+
 const Task = require('./task')
 
-test('create a task', assert => {
-  const task = Task.create('an toi voi me')
-  const today = Date.now()
+test('create a task with task notation', assert => {
+  const task = Task.create({description: 'an toi voi me'})
 
   assert.equals(task.description, 'an toi voi me')
-  assert.equals(task.createdAt, today)
+  assert.equals(task.createdAt, task.updatedAt)
+  assert.equals(task.completed, false)
+  assert.end()
+})
+
+test('create with task description only ', assert => {
+  const task = Task.create('an toi voi me')
+
+  assert.equals(task.description, 'an toi voi me')
   assert.equals(task.createdAt, task.updatedAt)
   assert.equals(task.completed, false)
   assert.end()
@@ -16,7 +24,11 @@ test('create a task', assert => {
 
 test('create a task, task is immutable', assert => {
   const task = Task.create('an toi voi me')
+  const today = Date.now()
 
   assert.throws(() => { task.description = 'do something' })
+  assert.throws(() => { task.createdAt = today })
+  assert.throws(() => { task.updatedAt = today })
+  assert.throws(() => { task.completed = true })
   assert.end()
 })
