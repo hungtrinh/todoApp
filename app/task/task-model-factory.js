9d25e1251f63f943347e25050ea16323be030d95
Trinh Hung <hungtd@vnext.vn>
2017-08-15 22:41:50 +0700

    More spec for task-model

--- app/task/task-model-factory.js
+++ app/task/task-model-factory.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// const assert = require('assert')
 /**
  * Create an task
  *
@@ -11,19 +12,40 @@
  *
  * @return {object} {description, createdAt, updatedAt, completed}
  */
-exports = module.exports = (task) => {
-  const today = Date.now()
-  const {
+exports = module.exports = taskModelFactory
+
+function taskModelFactory (task) {
+  const today = new Date()
+  let {
     description,
     createdAt,
     updatedAt,
     completed
   } = (typeof task === 'string') ? {description: task} : task
 
+  description = description || ''
+  createdAt = createdAt || today
+  updatedAt = updatedAt || today
+  completed = completed || false
+
   return Object.freeze({
-    description: description || '',
-    createdAt: createdAt || today,
-    updatedAt: updatedAt || today,
-    completed: completed || false
+    description: description,
+    createdAt: createdAt,
+    updatedAt: updatedAt,
+    completed: completed,
+    done: done
   })
+
+  function done () {
+    if (completed) {
+      throw new Error("task is completed before don't try do this task again")
+    }
+
+    return taskModelFactory({
+      description,
+      createdAt,
+      updatedAt: new Date(),
+      completed: true
+    })
+  }
 }
--- app/task/task-model.test.js
+++ app/task/task-model.test.js
@@ -4,31 +4,70 @@ const test = require('tape')
 
 const taskModelFactory = require('./task-model-factory')
 
-test('create a task with task notation', assert => {
+test('create a task with task notation', t => {
   const task = taskModelFactory({description: 'an toi voi me'})
 
-  assert.equals(task.description, 'an toi voi me')
-  assert.equals(task.createdAt, task.updatedAt)
-  assert.equals(task.completed, false)
-  assert.end()
+  t.equals(task.description, 'an toi voi me')
+  t.equals(task.createdAt, task.updatedAt)
+  t.equals(task.completed, false)
+  t.end()
 })
 
-test('create with task description only ', assert => {
+test('create with task description only ', t => {
   const task = taskModelFactory('an toi voi me')
 
-  assert.equals(task.description, 'an toi voi me')
-  assert.equals(task.createdAt, task.updatedAt)
-  assert.equals(task.completed, false)
-  assert.end()
+  t.equals(task.description, 'an toi voi me')
+  t.equals(task.createdAt, task.updatedAt)
+  t.equals(task.completed, false)
+  t.end()
 })
 
-test('create a task, task is immutable', assert => {
+test('create a task, task is immutable', t => {
   const task = taskModelFactory('an toi voi me')
   const today = Date.now()
 
-  assert.throws(() => { task.description = 'do something' })
-  assert.throws(() => { task.createdAt = today })
-  assert.throws(() => { task.updatedAt = today })
-  assert.throws(() => { task.completed = true })
-  assert.end()
+  t.throws(() => { task.description = 'do something' })
+  t.throws(() => { task.createdAt = today })
+  t.throws(() => { task.updatedAt = today })
+  t.throws(() => { task.completed = true })
+  t.end()
+})
+
+test('task.done() success will clone origin task with completed status', t => {
+  const createdDate = new Date()
+
+  const task = taskModelFactory({
+    description: 'an toi voi me',
+    createdAt: createdDate,
+    updatedAt: createdDate,
+    completed: false
+  })
+  const taskCompleted = task.done()
+
+  t.test('completed is true', ts => {
+    ts.true(taskCompleted.completed)
+    ts.end()
+  })
+  t.test('createdAt same with origin task', ts => {
+    ts.equals(taskCompleted.createdAt, task.createdAt)
+    ts.end()
+  })
+  t.test('updatedAt is current time', ts => {
+    ts.true(taskCompleted.updatedAt >= taskCompleted.createdAt)
+    ts.end()
+  })
+  t.test('description is same origin task', ts => {
+    ts.equals(taskCompleted.description, task.description)
+    ts.end()
+  })
+  t.end()
+})
+
+test('task.done() will raise exception when task is completed before', t => {
+  const task = taskModelFactory({
+    description: 'an toi voi me',
+    completed: true
+  })
+  t.throws(_ => task.done(), "task is completed before don't try do this task again")
+  t.end()
 })
