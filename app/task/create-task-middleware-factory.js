44365b0054e80f1c7147fed14e3e392e1fee54be
Trinh Hung <hungtd@vnext.vn>
2017-08-15 20:05:14 +0700

    Reduce code create-task-middleware-factory

--- app/task/create-task-middleware-factory.js
+++ app/task/create-task-middleware-factory.js
@@ -1,13 +1,7 @@
 
-module.exports = {
-  createTaskMiddlewareFactory
-}
-
-function createTaskMiddlewareFactory (taskRepository) {
-  return (ctx, next) => {
-    const taskDesc = ctx.request.body.task
-    ctx.assert(taskDesc, 400, 'description \'task\' required')
-    ctx.status = 201
-    ctx.body = taskRepository.create(taskDesc)
-  }
+module.exports = (taskRepository) => (ctx, next) => {
+  const taskDesc = ctx.request.body.task
+  ctx.assert(taskDesc, 400, 'description \'task\' required')
+  ctx.status = 201
+  ctx.body = taskRepository.create(taskDesc)
 }
--- app/task/init.js
+++ app/task/init.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const {createTaskMiddlewareFactory} = require('./create-task-middleware-factory')
+const createTaskMiddlewareFactory = require('./create-task-middleware-factory')
 const taskRepository = {create: taskDesc => ({id: 1, description: taskDesc})}
 module.exports = (router) => {
   router.post('/task', createTaskMiddlewareFactory(taskRepository))
